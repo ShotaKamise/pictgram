de358abefe5f282c25798a47b79166a5d3a5258b
name <email>
2019-12-08 15:04:20 +0000

    pictgram

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  validates :name, presence: true,
+                   length: {maximum: 15}
+  
+  VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates :email, presence: true, 
+                    format: {with: VALID_EMAIL_REGEX}
+  
+  
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}+\z/i
+  has_secure_password
+  validates :password, length: {in: 8..32},
+                      format: { with: VALID_PASSWORD_REGEX }
+  
+end
